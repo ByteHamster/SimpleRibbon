e841672b9ce698e09cac092cf7491333ba725466
ByteHamster <info@bytehamster.com>
2025-03-18 09:37:09 +0100

    Fix static assert

--- include/SimpleRibbon.h
+++ include/SimpleRibbon.h
@@ -7,10 +7,10 @@
 
 template<size_t bits, size_t coeff_bits = 64, typename result_t = uint8_t>
 class SimpleRibbon {
-        #define STATIC_LIBRARY_MESSAGE "This is a static library. \
-                            You can only select the template arguments that are compiled into the library."
+        #define STATIC_LIBRARY_MESSAGE "This is a static library. You can only select the template arguments that are compiled into the library."
         static_assert(coeff_bits == 32 || coeff_bits == 64 || (bits == 1 && coeff_bits == 128), STATIC_LIBRARY_MESSAGE);
         static_assert((bits <= 8 && std::is_same<result_t, uint8_t>::value)
+                        || (bits == 16 && coeff_bits == 64 && std::is_same<result_t, uint16_t>::value)
                         || (bits == 32 && coeff_bits == 64 && std::is_same<result_t, uint32_t>::value)
                         || (bits == 48 && coeff_bits == 64 && std::is_same<result_t, uint64_t>::value)
                         || (bits > 8 && bits <= 12 && std::is_same<result_t, uint16_t>::value), STATIC_LIBRARY_MESSAGE);
